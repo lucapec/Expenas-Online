6766c36e66b3663adf0a5aa971f9b4ce19e92587
Matias Sauan <matias@psiware.com.ar>
2022-08-25 21:09:14 -0300

    fixed post endpoint

--- Expenas-Online/AutoMapperProfiles/UnidadProfile.cs
+++ Expenas-Online/AutoMapperProfiles/UnidadProfile.cs
@@ -7,7 +7,9 @@ namespace ExpensasOnline.API.AutoMapperProfiles
         public UnidadProfile()
         {
             CreateMap<Entities.Unidad, Models.UnidadDto>();
+            CreateMap<Models.UnidadDto, Entities.Unidad>();
             CreateMap<Models.UnidadParaUpdateDto, Entities.Unidad>();
+            CreateMap<Entities.Unidad, Models.UnidadParaCrearDto>();
             CreateMap<Models.UnidadParaCrearDto, Entities.Unidad>();
         }
     }
